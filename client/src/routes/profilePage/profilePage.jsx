120949315c72dc97373ec5175b1d919b4de494a9
Itamar Ratson <itamar.ratson@gmail.com>
2024-09-07 22:45:40 +0300

    added fetching profile posts

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -2,13 +2,16 @@ import Chat from '../../components/chat/Chat';
 import List from '../../components/list/List';
 import './profilePage.scss';
 import apiRequest from '../../lib/apiRequest';
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { Await, Link, useLoaderData, useNavigate } from 'react-router-dom';
+import { Suspense, useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 function ProfilePage() {
+	const data = useLoaderData();
+
+	const { updateUser, currentUser } = useContext(AuthContext);
+
 	const navigate = useNavigate();
-	const { currentUser, updateUser } = useContext(AuthContext);
 
 	const handleLogout = async () => {
 		try {
@@ -19,7 +22,6 @@ function ProfilePage() {
 			console.log(err);
 		}
 	};
-
 	return (
 		<div className='profilePage'>
 			<div className='details'>
@@ -33,7 +35,7 @@ function ProfilePage() {
 					<div className='info'>
 						<span>
 							Avatar:
-							<img src={currentUser.avatar || '/noavatar.jpg'} alt='' />
+							<img src={currentUser.avatar || 'noavatar.jpg'} alt='' />
 						</span>
 						<span>
 							Username: <b>{currentUser.username}</b>
@@ -49,11 +51,19 @@ function ProfilePage() {
 							<button>Create New Post</button>
 						</Link>
 					</div>
-					<List />
+					<Suspense fallback={<p>Loading...</p>}>
+						<Await resolve={data.postResponse} errorElement={<p>Error loading posts!</p>}>
+							{(postResponse) => <List posts={postResponse.data.userPosts} />}
+						</Await>
+					</Suspense>
 					<div className='title'>
 						<h1>Saved List</h1>
 					</div>
-					<List />
+					<Suspense fallback={<p>Loading...</p>}>
+						<Await resolve={data.postResponse} errorElement={<p>Error loading posts!</p>}>
+							{(postResponse) => <List posts={postResponse.data.savedPosts} />}
+						</Await>
+					</Suspense>
 				</div>
 			</div>
 			<div className='chatContainer'>
