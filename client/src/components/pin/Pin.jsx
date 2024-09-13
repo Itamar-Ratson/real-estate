c6c1175d0e516e24d14ace84782708e9b1320808
Itamar Ratson <itamar.ratson@gmail.com>
2024-09-13 04:58:37 +0300

    bad import removed

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,15 +1,6 @@
 import { Marker, Popup } from 'react-leaflet';
 import './pin.scss';
 import { Link } from 'react-router-dom';
-import icon from 'react-leaflet/dist/images/marker-icon.png';
-import L from 'react-leaflet';
-import iconShadow from 'react-leaflet/dist/images/marker-shadow.png';
-let DefaultIcon = L.icon({
-	iconUrl: icon,
-	shadowUrl: iconShadow,
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
 
 function Pin({ item }) {
 	return (
