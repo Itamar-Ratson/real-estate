d71e60efad73690268c4e83944d86dbf866d0ac2
Itamar Ratson <itamar.ratson@gmail.com>
2024-09-13 11:49:29 +0300

    removed port 3000 from socketContext

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		setsocket(
-			io('https://estate.itamar.pro:4000', {
+			io('https://estate.itamar.pro', {
 				transports: ['websocket', 'polling'],
 			})
 		);
